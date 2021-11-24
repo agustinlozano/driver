8213829777aa0cc152e654c5795ab0970dc8e9e5
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2021-11-24 20:27:14 -0300

    Edit final file - Show information for the user with prompt messages - Add a sobroutine to do the calculation

--- final/calc_det.asm
+++ final/calc_det.asm
@@ -1,4 +1,10 @@
 %include "asm_io.inc"
+segment     .data
+            prompt1   db "Usted ingreso a = ", '0'
+            prompt2   db "   b = ", '0'
+            prompt3   db "   c = ", '0'
+            prompt4   db "   d = ", '0'
+            nonewline db "  ", '0'
 
 segment     .text
             global    calc_det
@@ -8,14 +14,47 @@ calc_det:
             pusha        ;aca lo quise hacer pusheando solo ebx
                          ;que fue lo que cambie. Pero no me funcionaba
 
+; imprimimos los prompt y los valores de cada argumento en la pila
+            mov   eax, prompt1
+            call  print_string
+            mov   eax, [ebp+8]
+            call  print_int
+
+            mov   eax, prompt2
+            call  print_string
+            mov   eax, [ebp+12]
+            call  print_int
+            
+            mov   eax, prompt3
+            call  print_string
+            mov   eax, [ebp+16]
+            call  print_int
+
+            mov   eax, prompt4
+            call  print_string
+            mov   eax, [ebp+20]
+            call  print_int
+
+            mov   eax, nonewline
+            call  print_string
+
             dump_stack 1, 2, 5
 
+; Llamo a la funcion que realiza el calculo
+            call   do_calc
+
+: Terminar el programa
+            popa
+            leave
+            ret
+
+do_calc:
             mov   dword eax, [ebp+8]
             imul  eax, [ebp+16]
             mov   [ebp-4], eax
 
             dump_stack 2, 2, 0
-
+            
             mov    dword eax, [ebp+20]
             imul   eax, [ebp+20]
             imul   eax, [ebp+12]
@@ -25,6 +64,4 @@ calc_det:
             mov    [ebp-8], ebx
             dump_stack 3, 2, 0
 
-            popa
-            leave
             ret
